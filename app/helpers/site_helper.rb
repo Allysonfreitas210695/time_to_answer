29b536f89692630ee5f6d6e1a276b074039f413d
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-01-17 13:46:25 -0300

    filtrando as questoes por assunto

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,13 @@
 module SiteHelper
+  def msg_container
+    case params[:action]
+      when 'index'
+        'Últimas perguntas Cadastrada....'
+      when 'questions'
+        "Resultando para ao tempo \'#{params[:term]}'\..."
+      when 'subject'
+        "Mostrando Questões para o assunto \'#{params[:subject]}'\..."
+      end
+  end
+  
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -5,8 +5,23 @@ class Question < ApplicationRecord
 
   # Kaminari
   paginates_per 5
+  
+  scope :_search_subject_, -> (page, subject_id) { 
+    Question.includes(:answers, :subject)
+            .where(subject_id: subject_id)
+            .page(page) 
+  }
 
-  scope :_search_, -> (page, term) { Question.includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page) }
+  scope :_search_, -> (page, term) { 
+    Question.includes(:answers, :subject)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page) 
+  }
+
     
-  scope :last_question, -> (page) { Question.includes(:answers).order('created_at desc').page(page) }
+  scope :last_question, -> (page) { 
+    Question.includes(:answers, :subject)
+            .order('created_at desc')
+            .page(page) 
+}
 end
