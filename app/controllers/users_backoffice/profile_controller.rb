597563b02890261e3ff6b7c57016671956cb66bc
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-01-19 23:40:52 -0300

    Trabalhando com radio b utton

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -1,11 +1,14 @@
 class UsersBackoffice::ProfileController < UsersBackofficeController
   before_action :set_user_profile, only: [:edit, :update]
   before_action :verify_password, only: [:update]
-  def edit; end
+
+  def edit
+    @user.build_user_profile if @user.user_profile.blank?
+  end
 
   def update
     if @user.update(params_user)
-       sign_in(@user, bypass: true)
+       bypass_sign_in(@user)
        redirect_to users_backoffice_profile_path, notice: 'Usuário atualizado com sucesso!'
     else
       render :edit
@@ -13,13 +16,14 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   end
 
   private
-  
+
   def set_user_profile
     @user = User.find(current_user.id)
   end
 
   def params_user
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
+                                  user_profile_attributes: [:id, :address, :gender, :birthdate])
   end
 
   def verify_password
--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -1,2 +1,18 @@
 module UsersBackofficeHelper
+  def gender_select(msg_user)
+    msg_format(msg_user)
+  end
+
+  private
+
+  def msg_format(msg)
+    case msg
+      when 'F'
+        'Feminino'
+      when 'M'
+        "Masculino"
+      else
+        ""
+      end
+  end
 end
