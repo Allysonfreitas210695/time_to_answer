85835243e04a996f7f59ec3d9dfc8512fd08b985
Allysonfreitas210695 <allysonfr83@gmail.com>
2022-01-22 23:14:31 -0300

    validacoes models

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,20 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
+  # mailtrap Config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'dd0fbef681d631',
+    :password => 'b32353f1b59d7e',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
